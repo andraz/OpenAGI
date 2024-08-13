b222d9e60c5796a6f0cec518944757db26ccc79a
Andraž Kos <andraz.kos@gmail.com>
2024-08-13 20:13:47 +0800

    fix pm2 config

--- pm2.config.js
+++ pm2.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   name: 'OpenAGI', // Name of your application
   script: 'frontend/index.js', // Entry point of your application
-  interpreter: '~/.bun/bin/bun', // Path to the Bun interpreter
+  interpreter: '/usr/local/bin/bun', // Path to the Bun interpreter
 }
