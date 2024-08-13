ea2f1026e54cb902f65cf94d66f20d5d861ad8de
Andraž Kos <andraz.kos@gmail.com>
2024-08-13 12:12:01 +0000

    init devcontainer env

---
+++ pm2.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'OpenAGI', // Name of your application
+  script: 'frontend/index.js', // Entry point of your application
+  interpreter: '~/.bun/bin/bun', // Path to the Bun interpreter
+}
